69725d3dde512a8e208e64bfd85d80e0daaa6bbf
Kevin Gaethofs <hello@kevingaethofs.be>
2020-06-26 18:15:05 +0200

    Viewport

--- index.js
+++ index.js
@@ -5,8 +5,12 @@ args.forEach(function (val, index, array) {
   (async () => {
     const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
     const page = await browser.newPage();
+    await page.setViewport({
+    width: 1200,
+    height: 1080
+    })
     await page.goto('https://instantlandpages.com/page/' + val);
-    await page.screenshot({path: 'screenshots/' + val + '.png'});
+    await page.screenshot({path: 'screenshots/' + val + '.jpg'});
 
     await browser.close();
   })();
